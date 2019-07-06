2fe8f6f27d3a728ccc432d338fac9e572403479b
N-C-M <45137335+N-C-M@users.noreply.github.com>
2019-07-06 20:47:54 +0530

    Update OddorEven.py

--- Odd/OddorEven.py
+++ Odd/OddorEven.py
@@ -1,10 +1,12 @@
+
 Task 
 Given an integer, , perform the following conditional actions:
 
 If  is odd, print Weird
 If  is even and in the inclusive range of  to , print Not Weird
 If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If  is even and greater than , print Not Weird 
+'''''''''''''''''''''''''''''''''''''''''''''''
 
 
 
