7ce7f4b5aaf891a911df442e875caf9f166d28a7
N-C-M <45137335+N-C-M@users.noreply.github.com>
2019-07-06 20:52:48 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+Task 
+Read an integer N. For all non-negative integers i<Nˆ2 , print iˆ2 . See the sample for details.
+..............................
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(n):
+ print(i*i)
